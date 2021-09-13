22b1162c631477c0d1c0ce6107ac60b82bdbf2e9
Shubham Singh <shubham88singh@gmail.com>
2021-09-13 19:39:06 +0530

    Adding the base layer and trying out the transform objects

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 1;
+mesh.position.y = -1;
+mesh.position.z = 1;
 
 scene.add(mesh);
 
