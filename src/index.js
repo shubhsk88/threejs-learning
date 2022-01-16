f095117e58a639b61e8774465e1bdff51ad2f7a8
Shubham <shubham88singh@gmail.com>
2022-01-16 15:46:40 +0530

    Adding the 3dtext file for deployment

--- src/index.js
+++ src/index.js
@@ -16,19 +16,77 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
+ * Axes helper
+ */
+
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+/**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
 
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial(),
-);
+const matCapTexture = textureLoader.load('textures/matcaps/8.png');
 
-scene.add(cube);
+/**
+ * Fonts
+ */
+
+const fontsLoader = new THREE.FontLoader();
+
+fontsLoader.load('fonts/helvetiker_regular.typeface.json', (font) => {
+  const textGeometry = new THREE.TextGeometry('Shubham Singh', {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+
+  textGeometry.computeBoundingBox();
+  textGeometry.center();
+
+  //   textGeometry.translate(
+  //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5,
+  //   );
+  const material = new THREE.MeshMatcapMaterial({ matcap: matCapTexture });
+
+  //   textMaterial.wireframe = true;
+  const text = new THREE.Mesh(textGeometry, material);
+  text.position.set(0, 0, 0);
+  scene.add(text);
+
+  const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+
+  for (let i = 0; i < 100; i += 1) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
+});
+// /**
+//  * Object
+//  */
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial(),
+// );
+
+// scene.add(cube);
 
 /**
  * Sizes
