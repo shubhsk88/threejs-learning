9ed7c2f25ee561a657cacb9047fe95f9655b95bb
Shubham <shubham88singh@gmail.com>
2022-01-24 21:03:38 +0530

    Trying out few shadows

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import { CameraHelper } from 'three';
 
 /**
  * Base
@@ -16,19 +17,74 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Textures
+ * Lights
  */
-const textureLoader = new THREE.TextureLoader();
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
+
+scene.add(ambientLight);
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
+directionalLight.position.set(2, 2, -1);
+
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+scene.add(directionalLight);
+directionalLight.castShadow = true;
+directionalLight.shadow.width = 1024;
+directionalLight.shadow.height = 1024;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.near = 2;
+directionalLight.shadow.camera.far = 6;
+// directionalLight.shadow.radius = 10;
+
+const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3);
+
+spotLight.shadow.width = 1024;
+spotLight.shadow.height = 1024;
+
+spotLight.castShadow = true;
+spotLight.position.set(0, 2, 2);
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+
+scene.add(spotLightCameraHelper);
+const directionalCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera,
+);
+directionalCameraHelper.visible = false;
+
+scene.add(directionalCameraHelper);
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.7;
+gui.add(material, 'metalness').min(0).max(1).step(0.001);
+gui.add(material, 'roughness').min(0).max(1).step(0.001);
 
 /**
- * Object
+ * Objects
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial(),
-);
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.castShadow = true;
 
-scene.add(cube);
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.5;
+
+plane.receiveShadow = true;
+
+scene.add(sphere, plane);
 
 /**
  * Sizes
@@ -49,6 +105,7 @@ window.addEventListener('resize', () => {
 
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
+
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
@@ -77,6 +134,9 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
