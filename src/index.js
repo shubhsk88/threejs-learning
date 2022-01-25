d40e6036ec2db145ed111ee991ca9ce0a820ca1d
Shubham <shubham88singh@gmail.com>
2022-01-25 23:12:27 +0530

    Adding the fake graves

--- src/index.js
+++ src/index.js
@@ -85,7 +85,26 @@ bush3.position.set(-0.8, 0.1, 2.1);
 
 house.add(bush1, bush2, bush3);
 
-// const gra
+const graves = new THREE.Group();
+
+scene.add(graves);
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({
+  color: '#b2b6b1',
+});
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 5;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0.4, z);
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  graves.add(grave);
+}
 
 // Floor
 const floor = new THREE.Mesh(
